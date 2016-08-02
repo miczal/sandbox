352be22db05cf3ad9f8c57ddcdf7e4c37e383b2e
miczal <mpierscinski@gmail.com>
2016-08-02 20:37:39 +0200

    [Morse]Refactoring

--- morse/test_morse.py
+++ morse/test_morse.py
@@ -4,11 +4,12 @@ from morse import MorseLatinTranslator
 
 class MorseLatinTranslatorTest(unittest.TestCase):
 
+    def setUp(self):
+        self.t = MorseLatinTranslator()
+
     def test_latin_separator_should_be_space(self):
-        t = MorseLatinTranslator()
-        self.assertEqual(t.latin_separator, ' ')
+        self.assertEqual(self.t.latin_separator, ' ')
 
     def test_morse_separator_should_be_vertical_line(self):
-        t = MorseLatinTranslator()
-        self.assertEqual(t.morse_separator, '|')
+        self.assertEqual(self.t.morse_separator, '|')
 
