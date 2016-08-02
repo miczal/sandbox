c4813b53462c6727af8a963e7a157baa8c85e3c0
miczal <mpierscinski@gmail.com>
2016-08-02 20:19:44 +0200

    [Morse]Assumptions for translator

---
+++ morse/morse.py
@@ -0,0 +1,12 @@
+
+class MorseLatinTranslator:
+    """
+    Simple two-way morse-latin translator
+    Assumptions:
+    - Morse characters separated by |
+    - Latin words separated by space
+    - Spaces between latin words are 'lost in translation'
+    - Latin characters translated from morse will be upper case
+    """
+    pass
+
